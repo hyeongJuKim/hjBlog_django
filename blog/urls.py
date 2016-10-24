90929c86b242b19ea75dd446f3ec16fe0d426b1c
HJ <hj@HJui-MacBook-Pro.local>
2016-10-24 12:00:09 +0900

    상세화면 추가

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
 	# 블로그 목록
     url(r'^$', views.post_list, name='post_list'),
 
+    # 블로그 상세화면
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Post
 
 # lte : less than equal		<=
@@ -11,3 +12,10 @@ from .models import Post
 def post_list(request):
 	posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+# post의 상세 화면
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
